ab6e4bdc6966808935b7f00072eea63e11a3fc17
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2018-10-20 18:54:04 -0500

    Update NDVI Calculation.py

--- NDVI Calculation.py
+++ NDVI Calculation.py
@@ -3,13 +3,13 @@ from osgeo import gdal
 from osgeo.gdalconst import*
 import matplotlib.pyplot as plt
 driver = gdal.GetDriverByName('GTiff')
-file_name = "D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif"
+file_name = "C:\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif" #debes cargar el archivo ráster de la banda 3
 dataset = gdal.Open(file_name, GA_ReadOnly)
 geotransform = dataset.GetGeoTransform()
 projection = dataset.GetProjection()
 band3 = dataset.GetRasterBand(1).ReadAsArray()
 dataset = None
-file_name = "D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif"
+file_name = "C:\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif" #debes cargar el archivo ráster de la banda 4
 dataset = gdal.Open(file_name, GA_ReadOnly)
 band4 = dataset.GetRasterBand(1).ReadAsArray()
 dataset = None
@@ -19,4 +19,7 @@ print(band3.dtype)
 ndvi = (band4-band3)/(band4+band3)
 plt.matshow(ndvi,cmap=plt.cm.jet, vmin=-1, vmax=1)
 plt.colorbar(shrink=0.8)
-plt.savefig('D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/NDVI.png', dpi=10)
+plt.savefig("C:\NDVI\indice.png", dpi=1000) #debes crear una carpeta en el disco del sistema operativo
+
+
+#Link de descarga de los archivos de banda: https://we.tl/t-DNgOFgGHte

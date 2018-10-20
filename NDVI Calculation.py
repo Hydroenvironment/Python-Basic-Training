bcba8b4cc62105a2da66280ea99e0c26001e6864
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2018-10-20 18:40:39 -0500

    Create NDVI Calculation.py

---
+++ NDVI Calculation.py
@@ -0,0 +1,22 @@
+from __future__ import division
+from osgeo import gdal
+from osgeo.gdalconst import*
+import matplotlib.pyplot as plt
+driver = gdal.GetDriverByName('GTiff')
+file_name = "D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif"
+dataset = gdal.Open(file_name, GA_ReadOnly)
+geotransform = dataset.GetGeoTransform()
+projection = dataset.GetProjection()
+band3 = dataset.GetRasterBand(1).ReadAsArray()
+dataset = None
+file_name = "D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif"
+dataset = gdal.Open(file_name, GA_ReadOnly)
+band4 = dataset.GetRasterBand(1).ReadAsArray()
+dataset = None
+print(geotransform)
+print(projection)
+print(band3.dtype)
+ndvi = (band4-band3)/(band4+band3)
+plt.matshow(ndvi,cmap=plt.cm.jet, vmin=-1, vmax=1)
+plt.colorbar(shrink=0.8)
+plt.savefig('D:/GEAHH/CONGRESO HIDROAMBIENTAL/PYTHON/Código-Hidrología/NDVI/NDVI.png', dpi=10)

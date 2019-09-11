dea7485bb61da8272ae36eed9969fddc8f7537ce
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2019-09-10 19:45:46 -0500

    Update Normal Depth-Manning.py

--- Normal Depth-Manning.py
+++ Normal Depth-Manning.py
@@ -1,21 +1,20 @@
 # importamos primeramente los módulos a usar
-from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import fmin
 
 # definimos variables
-n = 0.015
-S0 = 0.025
-Q = 9.26
-B = 2
+n = 0.015 #coeficiente de rugosidad de Manning
+S0 = 0.025 # Pendiente de fondo=línea de energía en flujo uniforme
+Q = 9.26 #Caudal que escurre por el canal
+B = 2 #Base del canal
 
 # definiendo la función para el cálculo del tirante normal
 def flow(y):
-    Q_estiamted = (1.49/n)*(S0**0.5)*((B*y)**(5/3))/((B+y)**(2/3))
-    epsilon = np.abs(Q_estiamted - Q)
+    Q_estimated = (1.49/n)*(S0**0.5)*((B*y)**(5/3))/((B+y)**(2/3))
+    epsilon = np.abs(Q_estimated - Q)
     return epsilon
-y_optimum = fmin(flow,0.5) 
+y_optimum = fmin(flow,0.1) 
 
 #veamos algunos detalles del cálculo
 print(y_optimum)

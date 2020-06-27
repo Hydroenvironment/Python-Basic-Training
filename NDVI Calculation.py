9c3f90d1e82bf432b87d651279bd503e84530370
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2020-06-27 23:55:19 +0200

    Update NDVI Calculation.py

--- NDVI Calculation.py
+++ NDVI Calculation.py
@@ -3,14 +3,14 @@ from osgeo import gdal
 from osgeo.gdalconst import*
 import matplotlib.pyplot as plt
 driver = gdal.GetDriverByName('GTiff')
-file_name = "C:\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif" #debes cargar el archivo ráster de la banda 3
-dataset = gdal.Open(file_name, GA_ReadOnly)
+file_name = "C:\\TEMPORAL\\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif" #debes cargar el archivo ráster de la banda 3
+dataset = gdal.Open("C:\\TEMPORAL\\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band3.tif",1)
 geotransform = dataset.GetGeoTransform()
 projection = dataset.GetProjection()
 band3 = dataset.GetRasterBand(1).ReadAsArray()
 dataset = None
-file_name = "C:\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif" #debes cargar el archivo ráster de la banda 4
-dataset = gdal.Open(file_name, GA_ReadOnly)
+file_name = "C:\\TEMPORAL\\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif" #debes cargar el archivo ráster de la banda 4
+dataset = gdal.Open("C:\\TEMPORAL\\LE07_L1TP_002071_20120211_20161204_01_T1_sr_band4.tif",1)
 band4 = dataset.GetRasterBand(1).ReadAsArray()
 dataset = None
 print(geotransform)
@@ -19,7 +19,5 @@ print(band3.dtype)
 ndvi = (band4-band3)/(band4+band3)
 plt.matshow(ndvi,cmap=plt.cm.jet, vmin=-1, vmax=1)
 plt.colorbar(shrink=0.8)
-plt.savefig("C:\NDVI\indice.png", dpi=1000) #debes crear una carpeta en el disco del sistema operativo
 
-
-#Link de descarga de los archivos de banda: https://we.tl/t-DNgOFgGHte
+#Acceso a archivos .tif: https://drive.google.com/drive/folders/1KX5c4gp1NY2M-Yi8Ztdlc7GukdsC9rU1?usp=sharing
